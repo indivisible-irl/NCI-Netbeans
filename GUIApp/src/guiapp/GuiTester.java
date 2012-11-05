5619cebe4371675c4fefa62d85756f50dae14e3f
indivisible-irl <mbspare@gmail.com>
2012-11-05 22:23:51 +0000

    week9, Basic GUI finished

---
+++ GUIApp/src/guiapp/GuiTester.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guiapp;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author DavidA
+ */
+public class GuiTester {
+    public static void main(String[] args)
+    {
+        //List<String> names = new ArrayList<>();
+        
+        PersonGUI pGUI = new PersonGUI();
+        pGUI.setVisible(true);
+    }
+    
+    
+    
+}
